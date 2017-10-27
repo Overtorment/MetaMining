0b57a6378c01582240bbd3a25befa8acd825517c
Overtorment <overtorment@gmail.com>
2017-10-28 00:22:56 +0300

    FIX

--- MetaToken.sol
+++ MetaToken.sol
@@ -1,26 +1,23 @@
 /*
 
-Meta Mining Meta Token
+Meta Mining Token
 
 https://github.com/Overtorment/MetaMining
 
 */
 
-import "https://github.com/OpenZeppelin/zeppelin-solidity/contracts/token/StandardToken.sol";
-import "https://github.com/OpenZeppelin/zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "https://github.com/OpenZeppelin/zeppelin-solidity/contracts/token/PausableToken.sol";
 
-import "./Ownable.sol";
+pragma solidity ^0.4.17;
 
-pragma solidity ^0.4.11;
-
-contract MetaToken is StandardToken, Ownable {
+contract MetaToken is PausableToken {
 
     string public name = 'MetaMetaMeta! Token';
     uint8 public decimals = 8;
     string public symbol = 'M3T';
-    string public version = '0.3.8';
+    string public version = '0.4.0';
 
-    uint256 public blockReward = 5 * (10**uint256(decimals));
+    uint256 public blockReward = 1 * (10**uint256(decimals));
     uint32 public halvingInterval = 210000;
     uint256 public blockNumber = 0; // how many blocks mined
     uint256 public totalSupply = 0;
@@ -29,13 +26,15 @@ contract MetaToken is StandardToken, Ownable {
     uint40 public lastMinedOn; // will be used to check how long did it take to mine
     uint256 public randomness;
 
+    address public newContractAddress;
+
     function MetaToken() Ownable() {
         lastMinedOn = uint40(block.timestamp);
         updateRandomness();
     }
 
     /// update randomness, will be used to find next Nonce
-    function updateRandomness() {
+    function updateRandomness() internal {
         randomness = uint256(sha3(sha3(uint256(block.blockhash(block.number-1)) + uint256(block.coinbase) + uint256(block.timestamp))));
     }
 
@@ -63,7 +62,7 @@ contract MetaToken is StandardToken, Ownable {
         accepts nonce aka "mining field", checks if it passess proof of work,
         rewards if it does
     */
-    function mine(uint256 nonce) returns (bool success) {
+    function mine(uint256 nonce) whenNotPaused returns (bool success) {
         require(checkMine(nonce));
 
         Mine(msg.sender, blockReward, uint40(block.timestamp) - uint40(lastMinedOn)); // issuing event to those who listens for it
@@ -91,14 +90,16 @@ contract MetaToken is StandardToken, Ownable {
         lastMinedOn = uint40(block.timestamp); // tracking time to check how much PoW took in the future
         if (blockNumber % halvingInterval == 0) { // time to halve reward?
             blockReward /= 2;
+            RewardHalved();
         }
 
         return true;
     }
 
-    function destroy() onlyOwner external {
-        selfdestruct(owner);
+    function setNewContractAddress(address newAddress) onlyOwner {
+        newContractAddress = newAddress;
     }
 
     event Mine(address indexed _miner, uint256 _reward, uint40 _seconds);
+    event RewardHalved();
 }
